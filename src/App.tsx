73eff59af36682d96d823a27cf2169c72c0ede45
Akash Singh <akashsingh11596@gmail.com>
2024-10-09 06:33:52 -0600

    Added the missing classes in @layer utilities.

--- src/App.tsx
+++ src/App.tsx
@@ -6,9 +6,9 @@
 // };
 
 // export default App;
-
+import "./globals.css";
 const App = () => {
-  return <div>App</div>;
+  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
 };
 
 export default App;
